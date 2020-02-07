4a70cc0a03347f2a0a23db8984846448470425b7
am-ui <59110493+am-ui@users.noreply.github.com>
2020-02-08 00:04:56 +0530

    basic python

---
+++ amitp.py
@@ -0,0 +1,10 @@
+# import pandas as pd
+import pandas as pd
+
+# list of strings
+lst = ['hii', 'i', 'am', 'amit',
+       'kumar', 'pathak', 'python']
+
+# Calling DataFrame constructor on list
+df = pd.DataFrame(lst)
+print(df)
